7fbc11bf4db1864e48ec142552dab0aa1cf8b5bb
wfrost1213 <116076379+wfrost1213@users.noreply.github.com>
2024-01-03 18:42:18 +0000

    Added ACAO Controls

--- index.js
+++ index.js
@@ -5,6 +5,34 @@ const _ = require("lodash");
 const { v4: uuid } = require("uuid");
 
 const app = express();
+const API_KEY = "rnd_A5jUpc4gRMRegFyvKAEhlvPmc6Z1";
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173, https://urban-liveability.me"
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.get("/outfit", (req, res) => {
   const tops = ["Black", "White", "Orange", "Navy"];
